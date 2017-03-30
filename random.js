ec1c55289778b2d35c91182235f16bee4979e590
nameless19922 <melikhov.dm@gmail.com>
2017-03-31 00:37:44 +0700

    add files

---
+++ random.js
@@ -0,0 +1,5 @@
+module.exports = (min, max) => { 
+    return Math.floor(
+		min + Math.random() * (max + 1 - min)
+	); 
+}
---
+++ transform-crypto.js
@@ -0,0 +1,20 @@
+const
+	Transform = require('stream').Transform,
+	crypto = require('crypto');
+
+module.exports = class TransfromCrypto extends Transform {
+	constructor(options) {
+		options = !options ? {} : options;
+		options.objectMode = true;
+
+		super(options);
+	}
+	
+	_transform(chunk, encoding, callback) {
+		let hash = chunk ? crypto.createHash('md5').update(chunk.toString()).digest('hex') : null;
+		
+		this.push(hash);
+		
+		callback();
+	}
+}
